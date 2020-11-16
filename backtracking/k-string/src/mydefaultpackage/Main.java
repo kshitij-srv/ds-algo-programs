9d31189447d09950db9e39215ce79ac377966eba
Kshitij Srivastava <kshitijsrv@gmail.com>
2020-11-16 23:17:43 +0530

    Modified backtracing k-ary string program to accept command-line args

--- backtracking/k-string/src/mydefaultpackage/Main.java
+++ backtracking/k-string/src/mydefaultpackage/Main.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 
 public class Main {
   public static void main(String args[]) {
-    int[] arr = new int[5];
-    kString(arr, 5, 10);
+    int n = args.length > 0 && args[0] != null ? Integer.parseInt(args[0]) : 5;
+    int k = args.length > 1 && args[1] != null ? Integer.parseInt(args[1]) : 2;
+    int[] arr = new int[n];
+    kString(arr, n, k);
   }
 
   static void kString(int[] arr, int n, int k) {
